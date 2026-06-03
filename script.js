b896579ad086d57266a2658e7abf798dc871a0bc
Erf92 <erf92raj@gmail.com>
2026-06-03 13:15:35 +0330

    handle html and css and js for weather app

---
+++ script.js
@@ -0,0 +1,62 @@
+const app = {
+  apiUrl: "https://api.weatherapi.com/v1/current.json",
+  apiKey: "ba0b69047d3849c499f170743260106",
+  searchForm: document.getElementById("searchForm"),
+  cityInput: document.getElementById("cityInput"),
+  weatherInfo: document.getElementById("weatherInfo"),
+};
+
+async function fetchWeather(city) {
+  app.weatherInfo.innerHTML = "<p>درحال دریافت اطلاعات...</p>";
+
+  try {
+    const response = await fetch(
+      `${app.apiUrl}?key=${app.apiKey}&q=${city}&aqi=no`
+    );
+
+    if (!response.ok) throw new Error(`خطا! وضعیت: ${response.status}`);
+
+    const data = await response.json();
+    if (data.error) throw new Error(data.error.message);
+
+    const variables = {
+      locationName: data.location.name,
+      conditionIcon: data.current.condition.icon,
+      tempCelsius: data.current.temp_c,
+      condition: data.current.condition.text,
+      humidity: data.current.humidity,
+    };
+
+    displayWeather(variables);
+  } catch (error) {
+    console.error("خطا در دریافت اطلاعات: ", error);
+    displayWeather(error.message);
+  }
+}
+
+function displayWeather(result) {
+  if (typeof result === "string") {
+    app.weatherInfo.innerHTML = `<p class="error-text">خطا: ${error.message}. لطفا دوباره تلاش کنید.</p>`;
+    return;
+  }
+
+  app.weatherInfo.innerHTML = `
+    <article class="weather__cart">
+      <img class="cart__icon" src="${result.conditionIcon}" alt="آیکون آب و هوا" />
+      <h2 class="cart__location">${result.locationName}</h2>
+      <span class="cart__temp">${result.tempCelsius} درجه سانتی گراد</span>
+      <span class="cart__condition">${result.condition}</span>
+      <span class="cart__humidity">رطوبت: ${result.humidity}</span>
+    </article>
+  `;
+}
+
+if (app.searchForm) {
+  app.searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const city = app.cityInput.value.trim();
+    if (city) fetchWeather(city);
+  });
+}
+
+fetchWeather("Tehran");
